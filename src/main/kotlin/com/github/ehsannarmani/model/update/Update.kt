fde6f13493ded0d97c4f39c99ddc45843919ba81
ehsan narmani <ehsan.enk.narmani@gmail.com>
2023-03-17 12:52:48 +0330

    add inline query update

--- src/main/kotlin/com/github/ehsannarmani/model/update/Update.kt
+++ src/main/kotlin/com/github/ehsannarmani/model/update/Update.kt
@@ -11,8 +11,29 @@ data class Update(
     val message: Message? = null,
     @SerialName("callback_query")
     val callbackQuery: CallbackQuery? = null,
+    @SerialName("inline_query")
+    val inlineQuery: InlineQuery? = null,
     @SerialName("my_chat_member")
     val myChatMember: MyChatMember? = null,
     @SerialName("poll_answer")
     val pollAnswer: PollAnswer? = null,
-)
+    var updateType: UpdateType = UpdateType.Message
+){
+    init {
+        updateType = if (message != null){
+            UpdateType.Message
+        }else if (callbackQuery != null){
+            UpdateType.CallbackQuery
+        }else if (inlineQuery != null){
+            UpdateType.InlineQuery
+        }else if (myChatMember != null){
+            UpdateType.ChatMember
+        }else{
+            UpdateType.PollAnswer
+        }
+    }
+}
+
+enum class UpdateType{
+    Message,CallbackQuery,InlineQuery,ChatMember,PollAnswer
+}
